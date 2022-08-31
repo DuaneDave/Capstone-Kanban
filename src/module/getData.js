722b7f62cf394849547f5f90aa2d41ad21806bb2
Duane David <duanedave17@gmail.com>
2022-08-31 00:27:39 -0700

    Update card length

--- src/module/getData.js
+++ src/module/getData.js
@@ -3,7 +3,7 @@ import cards from './cards.js';
 const getData = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const getJsonObj = await response.json();
-  const result = getJsonObj.slice(0, 21);
+  const result = getJsonObj.slice(0, 18);
   cards(result);
 };
 export default getData;
