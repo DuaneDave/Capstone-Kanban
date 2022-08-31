8fd20dad265437e2f9170b6eb05a1217320c6562
Duane David <duanedave17@gmail.com>
2022-08-31 03:00:07 -0700

    Implement movie counter

--- src/module/getData.js
+++ src/module/getData.js
@@ -1,9 +1,11 @@
 import cards from './cards.js';
+import countMovies from './counter.js';
 
 const getData = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const getJsonObj = await response.json();
   const result = getJsonObj.slice(0, 18);
   cards(result);
+  countMovies(result.length);
 };
 export default getData;
