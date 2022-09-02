4555bd5f85162c9ca5a81860eb8dfbf242d831b1
Duane David <duanedave17@gmail.com>
2022-09-02 08:53:26 -0700

    Update ui

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './style.css';
 import getData from './module/getData.js';
 
 getData();
+
+const hamburger = document.querySelector('.harmburger');
+const nav = document.querySelector('.nav-menu');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('slide');
+  nav.classList.toggle('slide');
+});
